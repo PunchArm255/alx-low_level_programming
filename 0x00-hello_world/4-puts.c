843e565322599824ae62fe0ac2d035b392aa3193
Simo <simohamednassiri@gmail.com>
2023-03-16 18:50:04 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - please work lay7r9 khtk
+ * Description: printf alt
+ * Return: 0 (success)
+ */
+int main(void) 
+{
+	puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
