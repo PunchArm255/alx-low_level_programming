20caf101be4943f5ab58ce08fe4b8d54781fcf40
Simo <simohamednassiri@gmail.com>
2023-03-16 19:40:29 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,14 @@
 #include<stdio.h>
-#include<unistd.h>
-
 /**
  * main - entry point
  *
- * Description: pain and suffer
- *
- * Return: always 1 (success)
+ * Description: pain and suffering its literally almost 4am
+ * Return: always 0 (Succcess)
  */
-
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	char s[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(s, sizeof(s), 1, stderr);
 	return (1);
 }
