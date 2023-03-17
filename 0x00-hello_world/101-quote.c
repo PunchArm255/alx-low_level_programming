cb5fac8ca406601c27d6a01cfcdb8948e05b790b
Simo <simohamednassiri@gmail.com>
2023-03-16 19:36:25 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,16 @@
 #include<stdio.h>
 #include<unistd.h>
+
 /**
  * main - entry point
  *
  * Description: pain and suffer
- * return: always 1 (success)
+ *
+ * Return: always 1 (success)
  */
+
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
