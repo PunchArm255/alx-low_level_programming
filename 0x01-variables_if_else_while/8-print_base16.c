c455b5a5e2a7da5d4052f7b263b599bbb7182000
Simo <simohamednassiri@gmail.com>
2023-03-17 18:35:05 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int digit = '0';
+
+	while (digit <= '9')
+		putchar(digit++);
+	digit = 'a';
+	while (digit <= 'f')
+		putchar(digit++);
+	putchar('\n');
+	return (0);
+}
