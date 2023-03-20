c054aa3ec69c090162fd5d7b9e08b13456740564
Simo <simohamednassiri@gmail.com>
2023-03-20 13:19:43 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int letter_1 = '0', letter_2;
+
+	while (letter_1 < '9')
+	{
+		letter_2 = letter_1 + 1;
+		while (letter_2 <= '9')
+		{
+			putchar(letter_1);
+			putchar(letter_2);
+			if (letter_1 != '8' || letter_2 != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			letter_2++;
+		}
+		letter_1++;
+	}
+	putchar('\n');
+	return (0);
+}
