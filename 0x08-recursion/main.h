68cf6c22e87e11a56c307ae3960be1048edc839d
Simo <simohamednassiri@gmail.com>
2023-04-05 19:44:56 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,32 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+void _puts_recursion(char *s);
+
+void _print_rev_recursion(char *s);
+
+int _strlen_recursion(char *s);
+
+int factorial(int n);
+
+int _pow_recursion(int x, int y);
+
+int sqqrt(float n, float l, float r);
+
+int _sqrt_recursion(int n);
+
+int prime(float n, float x);
+
+int is_prime_number(int n);
+
+int pal(char *s, int i, int j);
+
+int is_palindrome(char *s);
+
+int _sl(char *s);
+
+int wildcmp(char *s1, char *s2);
+
+#endif
