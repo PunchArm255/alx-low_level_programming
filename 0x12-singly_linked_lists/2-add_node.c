8bd396a87d2edc1ba390190cf90ff9b019b0bd9e
Simo Nassiri <68764731+PunchArm255@users.noreply.github.com>
2023-04-28 07:33:05 +0000

    task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,11 +4,11 @@
 #include <stdlib.h>
 
 /**
-  * _strlen - get a string's Length
+  * _Strlen - get a string's Length
   * @s: given string
   * Return: Length
   */
-size_t _strlen(const char *s)
+size_t _Strlen(const char *s)
 {
 	size_t size;
 
@@ -20,12 +20,12 @@ size_t _strlen(const char *s)
 }
 
 /**
-  * add_node - adds new node to head of list_t
+  * ins_node - adds new node to head of list_t
   * @head: head node
   * @str: node's string
   * Return: pointer to new head (Success) | NULL (Failure)
   */
-list_t *add_node(list_t **head, const char *str)
+list_t *ins_node(list_t **head, const char *str)
 {
 	char *s;
 	size_t size;
@@ -35,7 +35,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (!tmp)
 		return (0);
 	if (str)
-		s = strdup(str), size = _strlen(str);
+		s = strdup(str), size = _Strlen(str);
 	else
 		s = 0, size = 0;
 	tmp->str = s, tmp->len = size;
