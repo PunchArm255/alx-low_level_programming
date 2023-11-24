b622630a82bbf077821d7c4eb859f6c3be64fbba
PunchArm255 <simohamednassiri@gmail.com>
2023-11-23 17:27:33 -0800

    PLEASE WORK BRO HOLY

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,29 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef H
+#define H
 
-unsigned int binary_to_uint(const char *b);
+unsigned int binary_to_uint(const char *);
 
-#endif /* MAIN_H */
+int _putchar(char);
+
+void print_binary(unsigned long int);
+
+int get_bit(unsigned long int, unsigned int);
+
+int set_bit(unsigned long int *, unsigned int);
+
+void convert_to_binary(unsigned long int, unsigned short *);
+
+unsigned long int convert_to_decimal(unsigned short *);
+
+int clear_bit(unsigned long int *, unsigned int);
+
+void convert_to_binary2(unsigned long int, unsigned short *);
+
+unsigned long int convert_to_decimal2(unsigned short *);
+
+unsigned int flip_bits(unsigned long int, unsigned long int);
+
+void convert_to_binary3(unsigned long int, unsigned short *);
+
+int get_endianness(void);
+#endif /* endme */
