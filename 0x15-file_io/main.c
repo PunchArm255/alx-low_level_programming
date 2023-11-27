aaff44d73fce29bcb7a074edcbc3903fe53814fe
PunchArm255 <simohamednassiri@gmail.com>
2023-11-27 07:05:58 -0800

    pls work lol

--- 0x15-file_io/main.c
+++ 0x15-file_io/main.c
@@ -5,12 +5,12 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include<sys/stat.h>
+#include <sys/stat.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdarg.h>
 
-ssize_t read_textfile(const char *filename, size_t letters);
+size_t read_textfile(const char *filename, size_t letters);
 
 int create_file(const char *filename, char *text_content);
 
