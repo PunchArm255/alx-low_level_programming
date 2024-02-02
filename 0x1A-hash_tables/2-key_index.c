b847b00d7fc16942eea0efbeb1ad28adad5df799
PunchArm255 <simohamednassiri@gmail.com>
2024-02-02 11:31:58 -0800

    task 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,21 +1,14 @@
+#include <stdlib.h>
 #include "hash_tables.h"
 
-
-
 /**
- * key_index - Gives idx of a key
+ * key_index - map a ki to an index
+ * @key: given ki
+ * @size: size of tabla array
  *
- * @key: pointer to given key
- *
- * @size: array size of hash table
- *
- * Return: The idx of key in hash table
+ * Return: index
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int index = 0;
-
-    index = hash_djb2(key);
-    index %= size;
-    return (index);
+	return (hash_djb2(key) % size);
 }
